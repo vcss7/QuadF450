564eecec49d31bbcdb95cab4af4e3f8ba212c50b
vcss7 <vs.victorsolis@gmail.com>
2024-03-30 17:07:28 -0700

    Add .clang-format file and reformat main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
+#include "Adafruit_BME280.h"
+#include "Adafruit_BNO055.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+#include "Adafruit_Sensor.h"
 #include "Arduino.h"
 #include "Wire.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_BME280.h"
-#include "Adafruit_Sensor.h"
-#include "Adafruit_BNO055.h"
-#include "Adafruit_BME280.h"
-#include "Adafruit_SSD1306.h"
 
 // variables
 
 // functions prototypes
-void scanI2CBus();
+void scanI2CBus ();
 
 /* uncomment precompiler definitions as needed */
 // #define UTC602602_ENABLED     // display
@@ -21,212 +20,210 @@ void scanI2CBus();
 // display initialization
 #ifdef UTC602602_ENABLED
 constexpr byte UTC602602_ADDRESS = 0x3C;
-constexpr uint32_t SCREEN_WIDTH = 128;
+constexpr uint32_t SCREEN_WIDTH  = 128;
 constexpr uint32_t SCREEN_HEIGHT = 32;
-constexpr int8_t OLED_RESET = -1;
-Adafruit_SSD1306 utc602602(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+constexpr int8_t OLED_RESET      = -1;
+Adafruit_SSD1306 utc602602 (SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // utc function prototypes
-bool init_utc602602();
-void print_sensors_initialized(bool bno_init, bool bme_init, bool gps_init);
+bool init_utc602602 ();
+void print_sensors_initialized (bool bno_init, bool bme_init, bool gps_init);
 #endif
 
 // orientation sensor
 #ifdef BNO055_ENABLED
 // bno variable declarations
 constexpr byte BNO055_ADDRESS = 0x28;
-Adafruit_BNO055 bno055 = Adafruit_BNO055(BNO055_ADDRESS);
+Adafruit_BNO055 bno055        = Adafruit_BNO055 (BNO055_ADDRESS);
 imu::Quaternion quat;
 
 // bno function prototypes
-bool init_bno055();
-bool read_bno055_data();
+bool init_bno055 ();
+bool read_bno055_data ();
 #endif
 
 // press/humid/temp sensor
 #ifdef BME280_ENABLED
 // bme variable declarations
 // BME280_ADDRESS defined in Adafruit_BME280.h as 0x77
-Adafruit_BME280 bme280 = Adafruit_BME280();
+Adafruit_BME280 bme280 = Adafruit_BME280 ();
 // update to hPa of sea level for location and day for accurate altitude data
-constexpr float SEA_LEVEL_PRESSURE_HPA = 1013.25; 
+constexpr float SEA_LEVEL_PRESSURE_HPA = 1013.25;
 float temp;
 float pressure;
 float humidity;
 float altitude;
 
 // bme function prototypes
-bool init_bme280();
-bool read_bme280_data();
+bool init_bme280 ();
+bool read_bme280_data ();
 #endif
 
 // gps
-#ifdef ULTIMATEGPS_ENABLED      // device can take 63 bytes
+#ifdef ULTIMATEGPS_ENABLED  // device can take 63 bytes
 #endif
 
 /***** setup *****/
-void setup()
+void setup ()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Wire.begin();
-    Serial1.begin(9600);
+    pinMode (LED_BUILTIN, OUTPUT);
+    Wire.begin ();
+    Serial1.begin (9600);
 
-    bool bno055_initialized = false;
-    bool bme280_initialized = false;
+    bool bno055_initialized      = false;
+    bool bme280_initialized      = false;
     bool ultimategps_initialized = false;
 
 #ifdef UTC602602_ENABLED
-    init_utc602602();
+    init_utc602602 ();
 #endif
 #ifdef BNO055_ENABLED
-    bno055_initilized = init_bno055();
+    bno055_initilized = init_bno055 ();
 #endif
 #ifdef BME280_ENABLED
-    bme280_initialized = init_bme280();
+    bme280_initialized = init_bme280 ();
 #endif
 #ifdef ULTIMATEGPS_ENABLED
-    //Serial1.write("PMTK314,0,10,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
-    if(Serial1.available() > 0)
+    // Serial1.write("PMTK314,0,10,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
+    if (Serial1.available () > 0)
     {
         ultimategps_initialized = true;
     }
 #endif
 
 #ifdef UTC602602_ENABLED
-    print_sensors_initialized(bno055_initialized, bme280_initialized,
-                              ultimategps_initialized);
+    print_sensors_initialized (bno055_initialized, bme280_initialized,
+                               ultimategps_initialized);
 #endif
-    delay(1000);
-
+    delay (1000);
 }
 
 /***** loop *****/
-void loop()
+void loop ()
 {
-    digitalWrite(LED_BUILTIN, millis() % 1000 > 500);
+    digitalWrite (LED_BUILTIN, millis () % 1000 > 500);
 
-    //scanI2CBus();
+    // scanI2CBus();
 
 #ifdef BNO055_ENABLED
-    read_bno055_data();
+    read_bno055_data ();
 #endif
 #ifdef BME280_ENABLED
-    read_bme280_data();
+    read_bme280_data ();
 #endif
 #ifdef ULTIMATEGPS_ENABLED
-    if(Serial1.available())
+    if (Serial1.available ())
     {
 #ifdef UTC602602_ENABLED
-        utc602602.clearDisplay();
-        utc602602.setTextSize(1);
-        utc602602.setCursor(0, 0);
-        utc602602.setTextColor(WHITE);
-        utc602602.print("Serial1 available");
+        utc602602.clearDisplay ();
+        utc602602.setTextSize (1);
+        utc602602.setCursor (0, 0);
+        utc602602.setTextColor (WHITE);
+        utc602602.print ("Serial1 available");
 #endif
     }
 #endif
 }
 
-
-// functions 
-void scanI2CBus()
+// functions
+void scanI2CBus ()
 {
     byte error, address;
 
-    for(address = 1; address < 127; address++)
+    for (address = 1; address < 127; address++)
     {
-        digitalWrite(LED_BUILTIN, millis() % 200 > 100);
-        Wire.beginTransmission(address);
-        error = Wire.endTransmission();
+        digitalWrite (LED_BUILTIN, millis () % 200 > 100);
+        Wire.beginTransmission (address);
+        error = Wire.endTransmission ();
 
-        if(error == 0)
-            Serial.printf("Device found at Address 0x%02X\r\n", address);
+        if (error == 0)
+            Serial.printf ("Device found at Address 0x%02X\r\n", address);
         else
-            Serial.printf("No Device at Address 0x%02X\r\n", address);
+            Serial.printf ("No Device at Address 0x%02X\r\n", address);
 
-        delay(100);
+        delay (100);
     }
 }
 
 #ifdef UTC602602_ENABLED
-bool init_utc602602()
+bool init_utc602602 ()
 {
-    while(!utc602602.begin(SSD1306_SWITCHCAPVCC, UTC602602_ADDRESS))
+    while (!utc602602.begin (SSD1306_SWITCHCAPVCC, UTC602602_ADDRESS))
     {
-      Serial.print(
-          "UTC602602 Module not detected. Check your wiring or I2C ADDR!");
-      delay(1000);
+        Serial.print (
+            "UTC602602 Module not detected. Check your wiring or I2C ADDR!");
+        delay (1000);
     }
 
     // some defaults
-    utc602602.clearDisplay();
-    utc602602.setTextSize(1);
-    utc602602.setTextColor(WHITE);
-    utc602602.setCursor(0, 0);
-    utc602602.print("UTC Ready!");
-    utc602602.display();
-    delay(100);
+    utc602602.clearDisplay ();
+    utc602602.setTextSize (1);
+    utc602602.setTextColor (WHITE);
+    utc602602.setCursor (0, 0);
+    utc602602.print ("UTC Ready!");
+    utc602602.display ();
+    delay (100);
 
     return true;
 }
 
-void print_sensors_initialized(bool bno_init, bool bme_init, bool gps_init)
+void print_sensors_initialized (bool bno_init, bool bme_init, bool gps_init)
 {
     int16_t cursor_x = 0, cursor_y = 0;
 
-    utc602602.clearDisplay();
-    utc602602.setTextSize(1);
-    utc602602.setTextColor(WHITE);
-    utc602602.setCursor(cursor_x, cursor_y);
-    utc602602.print("SENSORS INITIALIZED");
+    utc602602.clearDisplay ();
+    utc602602.setTextSize (1);
+    utc602602.setTextColor (WHITE);
+    utc602602.setCursor (cursor_x, cursor_y);
+    utc602602.print ("SENSORS INITIALIZED");
 
 #ifdef BNO055_ENABLED
-    if(bno_init)
+    if (bno_init)
     {
         cursor_y += 10;
-        utc602602.setCursor(cursor_x, cursor_y);
-        utc602602.print("BNO055 Initialized!");
+        utc602602.setCursor (cursor_x, cursor_y);
+        utc602602.print ("BNO055 Initialized!");
     }
 #endif
 #ifdef BME280_ENABLED
-    if(bme_init)
+    if (bme_init)
     {
         cursor_y += 10;
-        utc602602.setCursor(cursor_x, cursor_y);
-        utc602602.print("BME280 Initialized!");
+        utc602602.setCursor (cursor_x, cursor_y);
+        utc602602.print ("BME280 Initialized!");
     }
 #endif
 #ifdef ULTIMATEGPS_ENABLED
-    if(gps_init)
+    if (gps_init)
     {
         cursor_y += 10;
-        utc602602.setCursor(cursor_x, cursor_y);
-        utc602602.print("UltimateGPS Initialized!");
+        utc602602.setCursor (cursor_x, cursor_y);
+        utc602602.print ("UltimateGPS Initialized!");
     }
 #endif
 
-    utc602602.display();
+    utc602602.display ();
 }
 #endif
 
 #ifdef BNO055_ENABLED
-bool init_bno055()
+bool init_bno055 ()
 {
-    while(!bno055.begin())
+    while (!bno055.begin ())
     {
-        Serial.print("No BNO055 detected. Check your wiring or I2C ADDR!");
-        delay(1000);
+        Serial.print ("No BNO055 detected. Check your wiring or I2C ADDR!");
+        delay (1000);
     }
-    delay(100);
+    delay (100);
 
-    bno055.setExtCrystalUse(true);
+    bno055.setExtCrystalUse (true);
 
     return true;
 }
 
-bool read_bno055_data()
+bool read_bno055_data ()
 {
-    quat = bno055.getQuat();
+    quat = bno055.getQuat ();
 
     // TODO: check if it reads data
     return true;
@@ -234,27 +231,26 @@ bool read_bno055_data()
 #endif
 
 #ifdef BME280_ENABLED
-bool init_bme280()
+bool init_bme280 ()
 {
-    while(!bme280.begin())
+    while (!bme280.begin ())
     {
-        Serial.print("No BME280 detected. Check your wiring or I2C ADDR!");
-        delay(1000);
+        Serial.print ("No BME280 detected. Check your wiring or I2C ADDR!");
+        delay (1000);
     }
-    delay(100);
+    delay (100);
 
     return true;
 }
 
-bool read_bme280_data()
+bool read_bme280_data ()
 {
-    temp = bme280.readTemperature();
-    pressure = bme280.readPressure();
-    humidity = bme280.readHumidity();
-    altitude = bme280.readAltitude(SEA_LEVEL_PRESSURE_HPA);
+    temp     = bme280.readTemperature ();
+    pressure = bme280.readPressure ();
+    humidity = bme280.readHumidity ();
+    altitude = bme280.readAltitude (SEA_LEVEL_PRESSURE_HPA);
 
     // TODO: check if it reads data
     return true;
 }
 #endif
-
