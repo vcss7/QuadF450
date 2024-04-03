5d364d55614ac97fb0b85908f89067ce227299e2
vcss7 <vs.victorsolis@gmail.com>
2024-04-03 00:55:16 -0700

    add basic initialization functions for uart device

--- lib/UltimateGPS/UltimateGPS.cpp
+++ lib/UltimateGPS/UltimateGPS.cpp
@@ -1,16 +1,62 @@
 #include "UltimateGPS.h"
 
+/**
+ * @brief Contrcutor to initialize the "device" without any connections.
+ */
 vcss::UltimateGPS::UltimateGPS ()
 {
 }
 
+/**
+ * @brief Constructor to initialize the "device" with a hardware serial
+ * connection.
+ *
+ * @param hardwareSerial is the HardwareSerial the device is connected to.
+ */
 vcss::UltimateGPS::UltimateGPS (HardwareSerial hardwareSerial)
 {
     this->gpsSerial = &hardwareSerial;
 }
 
-// class functions
+/**
+ * @brief Sets the HardwareSerial to use.
+ *
+ * @param hardwareSerial is the HardwareSerial the device is connected to.
+ */
 void vcss::UltimateGPS::setHardwareSerial (HardwareSerial hardwareSerial)
 {
     this->gpsSerial = &hardwareSerial;
+}
+
+/**
+ * @brief Sets rxPin.
+ *
+ * @param rxPin is the uint8_t to set the rxPin to.
+ */
+void vcss::UltimateGPS::setRxPin (const uint8_t rxPin)
+{
+    this->rxPin = rxPin;
+}
+
+/**
+ * @brief Sets txPin.
+ *
+ * @param txPin is the uint8_t to set the txPin to.
+ */
+void vcss::UltimateGPS::setTxPin (const uint8_t txPin)
+{
+    this->txPin = txPin;
+}
+
+/**
+ * @brief Checks if the rxPin is high.
+ *
+ * @details UART devices keep the transmitting line on high when idle. Returning
+ * true means *something* is keeping the line high.. maybe a UART device?
+ * 
+ * @return If rx line is high.
+ */
+bool vcss::UltimateGPS::isRxHigh ()
+{
+    return digitalRead (this->rxPin);
 }
--- lib/UltimateGPS/UltimateGPS.h
+++ lib/UltimateGPS/UltimateGPS.h
@@ -3,6 +3,26 @@
  *
  * @brief A lightweight library to get basic gps information from the Ultimate
  * GPS breakout module manufactured by Adafruit.
+ *
+ * MTK3339-based module specs:
+ * Satellites: 22 tracking, 66 searching
+ * Patch Antenna Size: 15mm x 15mm x 4mm
+ * Update rate: 1 to 10 Hz
+ * Position Accuracy: 1.8 meters
+ * Velocity Accuracy: 0.1 meters/s
+ * Warm/cold start: 34 seconds
+ * Acquisition sensitivity: -145 dBm
+ * Tracking sensitivity: -165 dBm
+ * Maximum Velocity: 515m/s
+ * Vin range: 3.0-5.5VDC
+ * MTK3339 Operating current: 25mA tracking, 20 mA current draw during
+ * navigation
+ * Output: NMEA 0183, 9600 baud default
+ * DGPS/WAAS/EGNOS supported
+ * FCC E911 compliance and AGPS support (Offline mode : EPO valid up to 14 days)
+ * Up to 210 PRN channels
+ * Jammer detection and reduction
+ * Multi-path detection and compensation
  */
 
 // The GPS modules uses NMEA
@@ -16,10 +36,23 @@
 namespace vcss
 {
 
+const uint8_t TEENSY_HARDWARE_SERIAL_1_RX = 0;
+const uint8_t TEENSY_HARDWARE_SERIAL_1_TX = 1;
+
+enum AckCmdResponse
+{
+    INVALID     = 0,
+    UNSUPPORTED = 1,
+    FAILED      = 2,
+    SUCCESS     = 3
+};
+
 class UltimateGPS
 {
 private:
-    HardwareSerial *gpsSerial;
+    HardwareSerial *gpsSerial = &SERIAL_PORT_HARDWARE;
+    uint8_t rxPin             = TEENSY_HARDWARE_SERIAL_1_RX;
+    uint8_t txPin             = TEENSY_HARDWARE_SERIAL_1_TX;
 
 public:
     // constructors
@@ -28,6 +61,15 @@ public:
 
     // class functions
     void setHardwareSerial (HardwareSerial hardwareSerial);
+    void setRxPin (const uint8_t rxPin);
+    void setTxPin (const uint8_t txPin);
+    bool isRxHigh ();
+
+    // pmkt functions
+    bool setGpsBaudRate (const uint32_t baudRate);
+    uint8_t checkGpsAck (const uint32_t cmd, const uint32_t);
+    void setNmeaOutput ();
+    bool validChecksum (const char *value, const uint8_t checksum);
 };
 
 }  // namespace vcss
